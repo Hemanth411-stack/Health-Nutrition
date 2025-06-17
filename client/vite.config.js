77fbc321aeed44c8efc74803fbdd3ce38e58e131
Hemanth411-stack <nagalahemanth411@gmail.com>
2025-06-17 16:34:20 +0530

    routes commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()],
-    build: {
+  plugins: [react()],
+  build: {
     outDir: 'dist',
   },
   base: '/',
 })
-
-
-// No need to import tailwindcss plugin like that — Tailwind works via `postcss.config.js`
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist',
-//   },
-//   base: '/', // 🔥 Ensures all routes and assets work correctly
-// })
